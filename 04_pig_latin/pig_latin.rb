a4829383b486fdc6f6ff04bb4fc0677b1cdf618d
erikbenton <erik.benton@hotmail.com>
2017-11-21 11:04:34 -0500

    completing ex04 - pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,16 @@
 #write your code here
+def translate(phrase)
+	words = phrase.split
+	vowels = ["a", "e", "i", "o", "u"]
+
+	words.map! do |word|
+		letters = word.split("")
+		while(not vowels.include? letters[0] or (letters.first == 'u' and letters.last == 'q'))
+			letter = letters.shift
+			letters.push(letter)
+		end
+		word = letters.join("") + "ay"
+	end
+
+	words.join(" ")
+end
