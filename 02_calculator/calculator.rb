f26acfa307bdeee8964b31b1ffc0b1aa4780cd2d
erikbenton <erik.benton@hotmail.com>
2017-11-21 09:14:00 -0500

    completing ex02 and adding TDD to rake file

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,37 @@
 #write your code here
+def add(x, y)
+	x+y
+end
+
+def subtract(x, y)
+	x-y
+end
+
+def sum(numbers)
+	sum = 0
+	numbers.each do |number|
+		sum += number
+	end
+	sum
+end
+
+def multiply(x,y, *more)
+	answer = x*y
+	more.each do |number|
+		answer = answer * number
+	end
+	answer
+end
+
+def power(x,y)
+	x**y
+end
+
+def factorial(x)
+	factorial = 1
+	while(x > 0)
+		factorial *= x
+		x -= 1
+	end
+	factorial
+end
