4f9aff8abc460a9b14d1425b23e26ccac7787d9c
erikbenton <erik.benton@hotmail.com>
2017-11-21 11:26:28 -0500

    completing ex05 - bookTitles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
 # write your code here
+
+	attr_accessor :title
+
+	def title=(to_be_title)
+		words_to_ignore = ["the", "and", "over", "in", "of", "a", "an"]
+		title_words = to_be_title.split
+		title_words.each do |word|
+			if(not words_to_ignore.include? word)
+				word.capitalize!
+			end
+		end
+		title_words[0].capitalize!
+		@title = title_words.join(" ")
+	end
+
 end
