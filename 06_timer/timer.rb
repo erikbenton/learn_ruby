e42d492c18850604148061621807246a791a3128
erikbenton <erik.benton@hotmail.com>
2017-11-21 13:49:43 -0500

    completing ex06 - timer with padded method

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,39 @@
 class Timer
   #write your code here
+
+  attr_accessor :seconds
+
+  def initialize
+
+  	@hours = 0
+  	@minutes = 0
+  	@seconds = 0
+
+
+  end
+
+  def seconds=(setSeconds)
+  	@seconds = (setSeconds%60)%60
+  	@minutes = (setSeconds/60)%60
+  	@hours = setSeconds/60/60
+  end
+
+  def time_string
+  	#Create time string
+  	timer_string = padded(@hours) + ":"
+  	timer_string += padded(@minutes) + ":"
+  	timer_string += padded(@seconds)
+  	timer_string
+
+  end
+
+  def padded(number)
+  	if number > 9
+  		string = "#{number}"
+  	else
+  		string = "0#{number}"
+  	end
+  	string
+  end
+
 end
